2e611e8cedb9bab492381e088bea454b979aec9f
ondoheer <ondoheer@gmail.com>
2019-09-08 22:11:38 -0500

    Quito console logs

--- managers/BulletManager.js
+++ managers/BulletManager.js
@@ -9,10 +9,8 @@ export class BulletManager {
     bullet.x = x;
     bullet.y = y;
     this.bulletsDisplayed.push(bullet);
-    console.log("bullet enabled");
   }
   disableBullet(bulletIndex) {
     this.bulletsPool.push(this.bulletsDisplayed.splice(bulletIndex, 1)[0]);
-    console.log("bullet disabled");
   }
 }
