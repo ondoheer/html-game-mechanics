efc34fdf55029968be96b572e421dc347a237f34
ondoheer <ondoheer@gmail.com>
2019-09-12 18:54:07 -0500

    Collision manager working

--- managers/CollisionManager.js
+++ managers/CollisionManager.js
@@ -3,9 +3,20 @@ export class CollisionManager {
     this.entities = entities;
   }
   areColliding(entity1, entity2) {
+    /**
+    The x position of the entity1 is greater than the x position of the entity2.
+    The x position of the entity1 is less than the x position of the entity2 plus its width.
+    The y position of the entity1 is greater than the y position of the entity2.
+    The y position of the entity1 is less than the y position of the entity2 plus its height.
+ */
     const entity1Area = entity1.getArea();
     const entity2Area = entity2.getArea();
-    if (entity1Area.x1 > entity2Area.x1 || entity1Area.x1 > entity2Area.x2) {
+    if (
+      entity1Area.x1 > entity2Area.x1 &&
+      entity1Area.x1 < entity2Area.x2 &&
+      entity1Area.y1 > entity2Area.y1 &&
+      entity1Area.y1 < entity2Area.y2
+    ) {
       console.log("went through");
     }
   }
