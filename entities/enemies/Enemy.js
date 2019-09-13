f3fb68b3e494da02b4bbb9dd03629b63af886ea1
ondoheer <ondoheer@gmail.com>
2019-09-13 07:29:30 -0500

    Intentando pensar como organizar las animaciones

--- entities/enemies/Enemy.js
+++ entities/enemies/Enemy.js
@@ -5,9 +5,14 @@ export class Enemy extends Entity {
   constructor(x, y, width, height, color, speed, hits, direction) {
     super(x, y, width, height, color, speed, direction);
     this.hits = hits;
+    this.hitAnimationTicking = 0;
   }
 
   isDead() {
     return this.hits <= 0;
+  }
+
+  hitAnimation() {
+    this.hitAnimationTicking += 1;
   }
 }
