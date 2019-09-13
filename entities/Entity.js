66cc1dcde7c55bdce323d711f34ee8010b1e3e7c
ondoheer <ondoheer@gmail.com>
2019-09-13 13:18:34 -0500

    Hacemos break si la bala choca con algo para que no siga iterando por las puras isColliding ahora retorna booleano

--- entities/Entity.js
+++ entities/Entity.js
@@ -11,11 +11,14 @@ export class Entity {
     this.yVelocity = 0;
     this.xVelocity = 0;
     this.height = height;
-    this.width = width; // this ought to be refactored around to use height and width instead of size
+    this.width = width;
     this.color = color;
     this.speed = speed;
     this.direction = direction;
   }
+  /**
+   * Returns the 4 points defining an entity area
+   */
   getArea() {
     const area = {
       x1: this.x,
@@ -25,7 +28,9 @@ export class Entity {
     };
     return area;
   }
-  // For diagonals this should be a state machine?
+  /**
+   * Based on the direction of the entity it sets the traveling speed for the X axis (so far)
+   */
   setTravelSpeed() {
     switch (this.direction) {
       case LEFT_DIRECTION:
@@ -38,12 +43,18 @@ export class Entity {
         break;
     }
   }
-
+  /**
+   * Changes x and y positions for the entity so it can be drawn in a new place
+   */
   travel() {
     this.x += this.xVelocity;
     this.y += this.yVelocity;
   }
-
+  /**
+   * Determines if this is colliding with another Entity entity
+   * (their areas overlap)
+   * @param {Entity} entity
+   */
   isColliding(entity) {
     const _thisArea = this.getArea();
     const entityArea = entity.getArea();
@@ -58,12 +69,21 @@ export class Entity {
       _thisArea.y2 < entityArea.y2
     ) {
       console.log("went through");
+      return true;
     }
+    return false;
   }
+  /**
+   * This updates the entity position and state so it can be drawn
+   */
   update() {
     this.setTravelSpeed();
     this.travel();
   }
+  /**
+   * This draws the entity in the canvas context
+   * @param {Canvas2dContext} ctx
+   */
   draw(ctx) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
--- managers/CollisionManager.js
+++ managers/CollisionManager.js
@@ -6,7 +6,9 @@ export class CollisionManager {
   bulletCollision(bullet) {
     for (let j = 0; j < this.entities.enemies.enemiesDisplayed.length; j++) {
       const enemy = this.entities.enemies.enemiesDisplayed[j];
-      bullet.isColliding(enemy);
+      if (bullet.isColliding(enemy)) {
+        break;
+      }
     }
   }
 }
