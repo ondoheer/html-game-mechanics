29403a818f4057dd1672e03850a5ef1192e420aa
ondoheer <ondoheer@gmail.com>
2019-09-15 09:21:19 -0500

    El planteamiento que he hecho de entities orchestrator no funciona,
    tengo que replantear todo

--- managers/EnemiesOrchestrator.js
+++ managers/EnemiesOrchestrator.js
@@ -10,21 +10,28 @@ export class EnemiesOrchestrator {
     this.enemies = entities.enemies;
     this.enemiesManager = enemiesManager;
     this.gameScript = LEVEL_STRUCTURE;
-    this._state = undefined;
+    this._state = {};
   }
   nextLevel() {
     state.currentLevel += 1;
   }
-  orchestrate() {
+  setUpLvl() {
     const currentLevelKey = `level${state.currentLevel}`;
-
     this._state = this.gameScript[currentLevelKey];
+    this._state.numberOfEnemiesInLvl = this.getNumberOfEnemies(currentLevelKey);
+    this._state.mapOfEnemies = this.createLvlEnemiesMap();
+  }
+  orchestrate() {
+    // Process the current level
+
+    /**
+     * hay que definir CUANDO aparecen los enemigos
+     * hay que definir donde aparecen los enemigos
+     * hay que hacer aparecer los enemigos
+     */
 
     switch (state.currentLevel) {
       case 1:
-        //while (condition) {}
-        //console.log(this.getNumberOfEnemies(currentLevelKey));
-        // TODO
         break;
 
       default:
@@ -40,20 +47,25 @@ export class EnemiesOrchestrator {
   }
   /**
    * Returns the types of enemies to be instantiated
+   * I think this function is useless
    * @param {key value in the form of "level{#}"} level
    */
   getTypesOfEnemies(level) {
     const enemies = this.getLevelEnemies(level);
-    return enemies;
+    const typesOfEnemies = enemies.map(({ type }) => type);
+    return typesOfEnemies;
   }
   /**
    *  Returns the sum of enemies in the level
+   * for it to be used in the general level counter
    * @param {key value in the form of "level{#}" }level
    */
   getNumberOfEnemies(level) {
     const enemies = this.getLevelEnemies(level);
-
-    const totalNumberOfEnemies = enemies.filter(enemy => enemy.number);
+    // get the total number of enemies that will be sent in this level
+    const totalNumberOfEnemies = enemies
+      .map(({ number }) => number)
+      .reduce((number, total) => number + total);
 
     return totalNumberOfEnemies;
   }
