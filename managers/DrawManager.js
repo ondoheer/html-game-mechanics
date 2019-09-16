90733810f5c5cb70f1ded8b201ded6782e0c88be
ondoheer <ondoheer@gmail.com>
2019-09-15 19:00:52 -0500

    Estrelliga de victoria

--- managers/DrawManager.js
+++ managers/DrawManager.js
@@ -9,6 +9,33 @@ export class DrawManager {
     this.enemiesManager = enemiesManager;
     this.hero = entities.hero;
   }
+  drawStar(cx, cy, spikes, outerRadius, innerRadius) {
+    var rot = (Math.PI / 2) * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.PI / spikes;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(cx, cy - outerRadius);
+    for (let i = 0; i < spikes; i++) {
+      x = cx + Math.cos(rot) * outerRadius;
+      y = cy + Math.sin(rot) * outerRadius;
+      this.ctx.lineTo(x, y);
+      rot += step;
+
+      x = cx + Math.cos(rot) * innerRadius;
+      y = cy + Math.sin(rot) * innerRadius;
+      this.ctx.lineTo(x, y);
+      rot += step;
+    }
+    this.ctx.lineTo(cx, cy - outerRadius);
+    this.ctx.closePath();
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeStyle = "gold";
+    this.ctx.stroke();
+    this.ctx.fillStyle = "yellow";
+    this.ctx.fill();
+  }
   /**
    * clears the canvas to draw the next iteration of it
    */
@@ -19,8 +46,11 @@ export class DrawManager {
    * Draws a you beat the game text
    */
   gameWon() {
+    this.clear();
     this.ctx.font = "50px sans";
-    this.ctx.fillText("You beat this game!!!!", 200, 90);
+    this.ctx.fillText("You Won!!!!", 250, 150);
+    this.ctx.fillStyle = "yellow";
+    this.drawStar(150, 120, 5, 50, 25);
   }
   /**
    * Draws a game over text
